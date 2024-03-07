829e9c43fe3733ac22f8832761f805238c68b2f5
drisspg <drisspguessous@gmail.com>
2024-03-06 19:33:44 -0800

    Skipping SAM for now since it hangs

--- float8_experimental/float8_dynamic_linear.py
+++ float8_experimental/float8_dynamic_linear.py
@@ -9,7 +9,7 @@ A wrapper around a `torch.nn.Linear` module which does fp8 compute.
 import torch
 
 from float8_experimental.float8_tensor import Float8Tensor, to_fp8_no_autograd
-from float8_experimental.float8_utils import tensor_to_scale
+from float8_experimental.float8_utils import IS_AMD, tensor_to_scale
 
 
 @torch._dynamo.allow_in_graph
@@ -30,10 +30,9 @@ class NoopFwToFloat8E5M2Bw(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, gradY):
-        gradY_scale = tensor_to_scale(gradY, torch.float8_e5m2)
-        fp8_tensor = to_fp8_no_autograd(
-            gradY, gradY_scale, torch.float8_e5m2, ctx.emulate
-        )
+        fp8_dtype = torch.float8_e5m2fnuz if IS_AMD else torch.float8_e5m2
+        gradY_scale = tensor_to_scale(gradY, fp8_dtype)
+        fp8_tensor = to_fp8_no_autograd(gradY, gradY_scale, fp8_dtype, ctx.emulate)
         return fp8_tensor, None
 
 
@@ -41,7 +40,7 @@ def cast_x_to_float8_e4m3fn_pre_hook(module, args):
     """
     Hook to cast the incoming activation to `torch.float8_e4m3fn`
     """
-    return module.cast_to_float8_e4m3fn(args[0])
+    return module.cast_to_float8_e4m3(args[0])
 
 
 def cast_grad_to_float8_e5m2_backward_forward_hook(module, input, output):
@@ -73,10 +72,10 @@ class Float8DynamicLinear(torch.nn.Linear):
 
     def forward(self, x):
         # cast x to float8_e4m3fn if not using activation hooks
-        x_fp8 = x if self.use_activation_hooks else self.cast_to_float8_e4m3fn(x)
+        x_fp8 = x if self.use_activation_hooks else self.cast_to_float8_e4m3(x)
 
         # cast w to float8_e4m3fn
-        w_fp8 = self.cast_to_float8_e4m3fn(self.weight)
+        w_fp8 = self.cast_to_float8_e4m3(self.weight)
 
         y = torch.nn.functional.linear(x_fp8, w_fp8, self.bias)
 
@@ -86,13 +85,31 @@ class Float8DynamicLinear(torch.nn.Linear):
 
         return y
 
-    def cast_to_float8_e4m3fn(self, inpt_tensor: torch.Tensor) -> Float8Tensor:
-        scale = tensor_to_scale(inpt_tensor, torch.float8_e4m3fn)
+    def cast_to_float8_e4m3(self, inpt_tensor: torch.Tensor) -> Float8Tensor:
+        """
+        This function casts the input tensor to a Float8Tensor
+        backed by one of two types depending on the GPU type
+
+        - On Nvidia GPUs, it casts to torch.float8_e4m3fn
+        - On AMD Gpus, it casts to torch.float8_e4m3fnuz
+
+        """
+        fp8_dtype = torch.float8_e4m3fnuz if IS_AMD else torch.float8_e4m3fn
+        scale = tensor_to_scale(inpt_tensor, fp8_dtype)
         return Float8Tensor.to_float8(
-            inpt_tensor, scale, torch.float8_e4m3fn, emulate=self.emulate
+            inpt_tensor, scale, fp8_dtype, emulate=self.emulate
         )
 
     def cast_to_float8_e5m2_bw(self, gradY: torch.Tensor) -> torch.Tensor:
+        """
+        This function is a noop in the forward but casts
+        the input tensor to a Float8Tensor during the backwards pass
+        backed by one of two types depending on the GPU type
+
+        - On Nvidia GPUs, it casts to torch.float8_e4m3fn
+        - On AMD Gpus, it casts to torch.float8_e4m3fnuz
+
+        """
         return NoopFwToFloat8E5M2Bw.apply(gradY, self.emulate)
 
     @classmethod
